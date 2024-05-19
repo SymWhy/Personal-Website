64af577a0c7e67363c00ed53d0ad75827a5b9d0e
SymWhy <simone.whysel@gmail.com>
2024-05-19 15:13:12 -0400

    restyle modal

--- gallery.js
+++ gallery.js
@@ -13,12 +13,29 @@ function myFunction(element) {
 // Add active class to the current thumbnail button (highlight it)
 function updateThumbs(element) {
     //get the active thumbnail
-    var current = modal.getElementsByClassName("active");
+    var current = modal.getElementsByClassName("modal-thumbs")[0].getElementsByClassName("active");
     //remove the "active" class from the previously active thumbnail
     current[0].className = current[0].className.replace(" active", "");
     //add the "active" class to this clicked thumbnail
     element.className += " active";
+
+    var myImg = document.getElementById(element.id.replace("-thumb", ""));
+
+    updateImage(myImg);
+    
 }
+
+function updateImage(element) {
+    //get the active image
+    var currentImg = modal.getElementsByClassName("modal-img-container")[0].getElementsByClassName("visible")[0];
+    //hide the previously active image
+    currentImg.className = currentImg.className.replace(" visible", "");
+    currentImg.style.display = "none";
+    //add the "visible" class to the new active image
+    element.className += " visible";
+    element.style.display = "block";
+}
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
