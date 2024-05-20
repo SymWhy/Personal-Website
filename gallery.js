4415e9c6664fe87a9231532d0835aa5d1223b865
SymWhy <simone.whysel@gmail.com>
2024-05-19 23:26:38 -0400

    finish gallery functionality

--- gallery.js
+++ gallery.js
@@ -1,26 +1,34 @@
 var currentIndex = 0;
 var images = null;
 var totalImages = null;
+var captions = null;
 
 
 // Open the lightbox
 function openLightbox(element, event) {
   imagesContainer = document.getElementById('modal-img-' + element.id);
-  console.log(imagesContainer);
   images = imagesContainer.querySelectorAll("IMG");
-  // images = document.querySelectorAll('.modal-img-' + element.id).getElementsByType("IMG");
-  console.log(images);
   totalImages = images.length;
 
   currentIndex = 0;
 
+  captions = document.getElementById("modal-captions-" + element.id).getElementsByClassName("modal-caption");
+
   updateLightboxImage();
+
+  var myText = document.getElementById("modal-text-" + element.id).innerHTML;
+
+  document.getElementById("modal-text").innerHTML = myText;
 
   document.getElementById("modal").style.display = "block";
 }
 
 // Close the lightbox
 function closeLightbox() {
+  //Clear existing text
+  document.getElementById('modal-text').innerHTML = "";
+
+  //close the modal
   document.getElementById('modal').style.display = 'none';
 }
 
@@ -38,17 +46,25 @@ function changeImage(direction) {
 // Update the lightbox image and thumbnails
 function updateLightboxImage() {
   const lightboxImg = document.getElementById('modal-img');
+  const captionContainer = document.getElementById('modal-caption');
   const thumbnailContainer = document.getElementById('modal-thumbs');
 
   // Update the main lightbox image
   lightboxImg.src = images[currentIndex].src;
+
+  //update captions
+  captionContainer.innerHTML = "";
+  
+  var myCaption = captions[currentIndex].innerHTML;
+  captionContainer.innerHTML = myCaption;
+
 
   // Clear existing thumbnails
   thumbnailContainer.innerHTML = '';
 
   // Add new thumbnails
   images.forEach((image, index) => {
-    console.log(image);
+
     const thumbnail = document.createElement('img');
     thumbnail.src = image.src;
     thumbnail.alt = `Thumbnail ${index + 1}`;
