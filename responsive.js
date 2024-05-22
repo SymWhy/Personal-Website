8bc028223dc707636a676bebe52a2f44f69dedaf
SymWhy <simone.whysel@gmail.com>
2024-05-22 12:53:16 -0400

    make columns responsive

---
+++ responsive.js
@@ -0,0 +1,15 @@
+var e = document.getElementById("filter-div-container");
+
+window.addEventListener("resize", ( () =>  {
+    if (window.innerWidth > 1200) {
+        e.style.setProperty("grid-template-columns", "repeat(3, 1fr)");
+    }
+
+    else if (window.innerWidth > 800) {
+        e.style.setProperty("grid-template-columns", "repeat(2, 1fr)");
+    }
+
+    else {
+        e.style.setProperty("grid-template-columns", "repeat(1, 1fr)");
+    }
+}))
